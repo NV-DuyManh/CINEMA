b04b28c8166b8ba16a2d44da0407e2e71a898919
Nguyen Van Duy Manh <154242112+NV-DuyManh@users.noreply.github.com>
2026-03-01 23:38:30 +0700

    Manh update WelcomePage

--- Cinema/Cinema/WelcomePage.xaml.cs
+++ Cinema/Cinema/WelcomePage.xaml.cs
@@ -1,28 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cinema
 {
-    /// <summary>
-    /// Interaction logic for WelcomePage.xaml
-    /// </summary>
     public partial class WelcomePage : Page
     {
+    
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBRapPhim.mdf;Integrated Security=True;Connect Timeout=30";
+
         public WelcomePage()
         {
             InitializeComponent();
+            this.Loaded += WelcomePage_Loaded;
+        }
+
+        private void WelcomePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadThongKeTuDatabase();
+        }
+
+        private void LoadThongKeTuDatabase()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    string queryPhim = "SELECT COUNT(*) FROM phim WHERE trang_thai = 'DangChieu'";
+                    using (SqlCommand cmd = new SqlCommand(queryPhim, conn))
+                    {
+                        int phimCount = Convert.ToInt32(cmd.ExecuteScalar());
+                        TxtPhimCount.Text = phimCount.ToString("D2"); 
+                    }
+
+                    string queryLichChieu = "SELECT COUNT(*) FROM lichchieu WHERE ngay_chieu = CAST(GETDATE() AS DATE)";
+                    using (SqlCommand cmd = new SqlCommand(queryLichChieu, conn))
+                    {
+                        int lichChieuCount = Convert.ToInt32(cmd.ExecuteScalar());
+                        TxtLichChieuCount.Text = lichChieuCount.ToString("D2");
+                    }
+
+                    string querySanPham = "SELECT COUNT(*) FROM sanpham WHERE trang_thai = 'DangBan'";
+                    using (SqlCommand cmd = new SqlCommand(querySanPham, conn))
+                    {
+                        int sanPhamCount = Convert.ToInt32(cmd.ExecuteScalar());
+                        TxtSanPhamCount.Text = sanPhamCount.ToString("D2");
+                    }
+
+                    string queryPhong = "SELECT COUNT(*) FROM phongchieu WHERE tinh_trang = 'HoatDong'";
+                    using (SqlCommand cmd = new SqlCommand(queryPhong, conn))
+                    {
+                        int phongCount = Convert.ToInt32(cmd.ExecuteScalar());
+                        TxtPhongCount.Text = phongCount.ToString("D2");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải dữ liệu thống kê: " + ex.Message, "Lỗi kết nối", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
